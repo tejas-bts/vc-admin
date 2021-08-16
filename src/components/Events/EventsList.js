7f0548468687bf33f1560741ef24a43f72fbc49c
Aditya Kale <akale@biztransights.com>
2021-08-16 21:12:25 +0530

    Create Event Resolved

--- src/components/Events/EventsList.js
+++ src/components/Events/EventsList.js
@@ -1,204 +1,273 @@
-import React, { useEffect, useState } from 'react'
-import ReactPaginate from 'react-paginate';
-import EventItem from './Event.ListItem';
-import { Link } from 'react-router-dom';
-import { getAllCategories } from '../../services/category.service'
-import { FiArrowDown, FiArrowUp, FiChevronDown, FiSearch } from 'react-icons/fi';
-import { getAllEvents } from '../../services/events.services';
-import Spinner from '../../components/core/Spinner';
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import EventItem from "./Event.ListItem";
+import { Link } from "react-router-dom";
+import { getAllCategories } from "../../services/category.service";
+import {
+  FiArrowDown,
+  FiArrowUp,
+  FiChevronDown,
+  FiSearch,
+} from "react-icons/fi";
+import { getAllEvents } from "../../services/events.services";
+import Spinner from "../../components/core/Spinner";
 
 const initalSearchParams = {
-    "eventTitle" : "",
-    "eventStartDate" : "",
-    "eventCategoryId" : "",
-    "eventType" : "",
-    "eventNature" : "",
-    "hostType" : "",
-    "hostId" : "",
-    "presenterType" : "",
-    "presenterId" : "",
-    "limit" : "",
-    "offset" : "",
-    "sortCol" : "",
-    "sortOrder" : ""
-  }
+  eventTitle: "",
+  eventStartDate: "",
+  eventCategoryId: "",
+  eventType: "",
+  eventNature: "",
+  hostType: "",
+  hostId: "",
+  presenterType: "",
+  presenterId: "",
+  limit: "",
+  offset: "",
+  sortCol: "",
+  sortOrder: "",
+};
 
-function EventList({match}) {
+function EventList({ match }) {
+  const [loading, setLoading] = useState(true);
+  const [categoryOptions, setOptions] = useState([]);
+  const [organizations, setOrganizations] = useState([]);
+  const [displayList, setDisplayList] = useState([]);
+  const [searchParams, setSearchParams] = useState(initalSearchParams);
+  const [listAttributes, setListAttributes] = useState({
+    pageNumber: 0,
+    pageSize: 5,
+    pageCount: 0,
+    sortBy: "Zipcode",
+    sortDirection: true,
+  });
 
-    const [ loading, setLoading ] = useState(true);
-    const [ categoryOptions, setOptions ] = useState([]);
-    const [ organizations, setOrganizations ] = useState([]);
-    const [ displayList, setDisplayList ] = useState([]);
-    const [ searchParams, setSearchParams ] = useState(initalSearchParams);
-    const [ listAttributes, setListAttributes ] = useState({ pageNumber : 0, pageSize: 5, pageCount: 0, sortBy: 'Zipcode', sortDirection: true })
+  const handleSort = (e) => {
+    // console.log(e.target.getAttribute('column'));
+    const column = e.target.getAttribute("column");
+    let currentListAttributes = listAttributes;
+    if (currentListAttributes.sortBy === column)
+      currentListAttributes = {
+        ...currentListAttributes,
+        sortDirection: !listAttributes.sortDirection,
+      };
+    setListAttributes({ ...currentListAttributes, sortBy: column });
+  };
 
-    const handleSort = (e) => {
-      console.log(e.target.getAttribute('column'));
-      const column = e.target.getAttribute('column');
-      let currentListAttributes = listAttributes;
-      if(currentListAttributes.sortBy === column) currentListAttributes = {...currentListAttributes, sortDirection: !listAttributes.sortDirection };
-      setListAttributes({...currentListAttributes, sortBy:column})
-    }
+  useEffect(() => {
+    getAllCategories().then((response) => setOptions(response.data));
+    getAllEvents(searchParams).then((response) => {
+      // console.log("Events List",response)
+      const organizations = response.data;
+      setOrganizations(organizations);
+      const newListAttributes = { ...listAttributes };
+      newListAttributes.pageCount = Math.ceil(
+        organizations.length / listAttributes.pageSize
+      );
+      setListAttributes(newListAttributes);
+      setLoading(false);
+    });
+  }, []);
 
-    useEffect(() => {
-      getAllCategories()
-        .then((response) => setOptions(response.data));
-      getAllEvents(searchParams)
-        .then((response) => {
-          console.log("Events List",response)
-          const organizations = response.data
-          setOrganizations(organizations);
-          const newListAttributes = {...listAttributes};
-          newListAttributes.pageCount = Math.ceil(organizations.length/listAttributes.pageSize);
-          setListAttributes(newListAttributes);
-          setLoading(false);
-        })
-    }, [])
+  useEffect(() => {
+    console.log("Display List", displayList);
+  }, [displayList]);
 
-    useEffect(() => {
-      console.log('Display List', displayList);
-    },[displayList])
+  useEffect(() => {
+    console.log("List Attributes", listAttributes);
+    const start =
+      parseInt(listAttributes.pageNumber) * parseInt(listAttributes.pageSize);
+    const end = start + listAttributes.pageSize;
+    let orgList = [...organizations];
 
-    useEffect(() => {
-      console.log('List Attributes',listAttributes)
-      const start = parseInt(listAttributes.pageNumber) * parseInt(listAttributes.pageSize);
-      const end = start + listAttributes.pageSize;
-      let orgList = [...organizations];
+    if (listAttributes.sortBy !== undefined) {
+      const column = listAttributes.sortBy;
 
-      if(listAttributes.sortBy !== undefined) {
-        const column = listAttributes.sortBy;
-
-        if(listAttributes.sortDirection) orgList.sort((a,b) => {
+      if (listAttributes.sortDirection)
+        orgList.sort((a, b) => {
           if (a[column] < b[column]) return -1;
           if (a[column] > b[column]) return 1;
           return 0;
-        })
-        else orgList.sort((a,b) => {
+        });
+      else
+        orgList.sort((a, b) => {
           if (a[column] < b[column]) return 1;
           if (a[column] > b[column]) return -1;
           return 0;
-        })
-      }
+        });
+    }
 
-      const displayList = [...orgList.slice(start,end)];
-      setDisplayList(displayList);
+    const displayList = [...orgList.slice(start, end)];
+    setDisplayList(displayList);
+  }, [listAttributes, organizations]);
 
-    }, [listAttributes,organizations])
-
-    return (
-        <div className="settings-wrapper">
-            <div className="list-controls">
-                <Link to={`${match.path}new`} style={{float:'right'}}>
-                        <button className="button is-solid accent-button">New Event</button>
-                </Link>
-                <h1 className="admin-title">Events</h1>
-            </div>
-            <div className="list-controls">
-                <div className="small-input">
-                  <button className="input is-rounded admin-search-button" placeholder="Type" > <FiSearch className="mr-2"/>Search</button>
-                </div>
-                <div className="small-input">
-                    <input className="input is-rounded" type="text" placeholder="Name" />
-                    <div className="search-icon">
-                        <FiSearch />
-                    </div>
-                </div>
-                <div className="small-input">
-                    <input className="input is-rounded" type="text" placeholder="Email" />
-                    <div className="search-icon">
-                        <FiSearch />
-                    </div>
-                </div>
-                <div className="small-input">
-                    <input className="input is-rounded" type="text" placeholder="City" />
-                    <div className="search-icon">
-                        <FiSearch />
-                    </div>
-                </div>
-                <div className="small-input">
-                    <input className="input is-rounded" type="text" placeholder="State" />
-                    <div className="search-icon">
-                        <FiSearch />
-                    </div>
-                </div>
-                <div className="small-input">
-                    <input className="input is-rounded" type="text" placeholder="Type" />
-                    <div className="search-icon">
-                        <FiSearch />
-                    </div>
-                </div>
-                <div className="small-input">
-                    <select className="input is-rounded" type="text" style={{paddingLeft:'30px', textAlign: 'center'}}>
-                      <option disabled selected value>Select  Category</option>
-                      {categoryOptions.map((item) => <option>{item.CategoryName}</option>)}
-                    </select>
-                    <div className="search-icon">
-                        <FiChevronDown />
-                    </div>
-                </div>
-            </div>
-            <div class="flex-table">
-                <div class="flex-table-header">
-                    <span class="name sort-column" onClick={handleSort} column="OrgName">
-                      Name
-                      {
-                        listAttributes.sortBy === "OrgName" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="location sort-column" onClick={handleSort} column="EventStartDateTime">
-                      Date
-                      {
-                        listAttributes.sortBy === "EventStartDateTime" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="type sort-column" onClick={handleSort} column="OrgType">
-                      Type
-                      {
-                        listAttributes.sortBy === "OrgType" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="category sort-column" onClick={handleSort} column="PresenterType">
-                      Presenter Type
-                      {
-                        listAttributes.sortBy === "PresenterType" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="events-count sort-column" onClick={handleSort} column="EventNature">
-                      Event Nature
-                      {
-                        listAttributes.sortBy === "EventNature" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="status sort-column" onClick={handleSort} column="PresenterType">
-                    Presenter Type
-                      {
-                        listAttributes.sortBy === "PresenterType" && 
-                        (listAttributes.sortDirection ? <FiArrowUp className="ml-2"/> : <FiArrowDown className="ml-2"/>)
-                      }
-                    </span>
-                    <span class="edit sort-column" column="PresenterType" >Edit</span>
-                </div>
-                {loading ? <Spinner /> : displayList.map((item) => <EventItem event={item} key={item.OrgId} match={match} />)}
-            </div>
-            <ReactPaginate
-                previousLabel={'Prev'}
-                nextLabel={'Next'}
-                breakLabel={'...'}
-                breakClassName={'break-me'}
-                pageCount={listAttributes.pageCount}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={3}
-                onPageChange={(x) => setListAttributes({...listAttributes, pageNumber: x.selected})}
-                containerClassName={'pagination'}
-                activeClassName={'active'}
-            />
+  return (
+    <div className="settings-wrapper">
+      <div className="list-controls">
+        <Link to={`${match.path}new`} style={{ float: "right" }}>
+          <button className="button is-solid accent-button">New Event</button>
+        </Link>
+        <h1 className="admin-title">Events</h1>
+      </div>
+      <div className="list-controls">
+        <div className="small-input">
+          <button
+            className="input is-rounded admin-search-button"
+            placeholder="Type">
+            {" "}
+            <FiSearch className="mr-2" />
+            Search
+          </button>
         </div>
-    )
+        <div className="small-input">
+          <input className="input is-rounded" type="text" placeholder="Name" />
+          <div className="search-icon">
+            <FiSearch />
+          </div>
+        </div>
+        <div className="small-input">
+          <input className="input is-rounded" type="text" placeholder="Email" />
+          <div className="search-icon">
+            <FiSearch />
+          </div>
+        </div>
+        <div className="small-input">
+          <input className="input is-rounded" type="text" placeholder="City" />
+          <div className="search-icon">
+            <FiSearch />
+          </div>
+        </div>
+        <div className="small-input">
+          <input className="input is-rounded" type="text" placeholder="State" />
+          <div className="search-icon">
+            <FiSearch />
+          </div>
+        </div>
+        <div className="small-input">
+          <input className="input is-rounded" type="text" placeholder="Type" />
+          <div className="search-icon">
+            <FiSearch />
+          </div>
+        </div>
+        <div className="small-input">
+          <select
+            className="input is-rounded"
+            type="text"
+            style={{ paddingLeft: "30px", textAlign: "center" }}>
+            <option disabled selected value>
+              Select Category
+            </option>
+            {categoryOptions.map((item) => (
+              <option>{item.CategoryName}</option>
+            ))}
+          </select>
+          <div className="search-icon">
+            <FiChevronDown />
+          </div>
+        </div>
+      </div>
+      <div class="flex-table">
+        <div class="flex-table-header">
+          <span
+            class="name sort-column"
+            onClick={handleSort}
+            column="eventTitle">
+            Name
+            {listAttributes.sortBy === "eventTitle" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span
+            class="location sort-column"
+            onClick={handleSort}
+            column="EventStartDateTime">
+            Date
+            {listAttributes.sortBy === "EventStartDateTime" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span
+            class="type sort-column"
+            onClick={handleSort}
+            column="eventTitle">
+            Type
+            {listAttributes.sortBy === "eventTitle" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span
+            class="category sort-column"
+            onClick={handleSort}
+            column="PresenterType">
+            Presenter Type
+            {listAttributes.sortBy === "PresenterType" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span
+            class="events-count sort-column"
+            onClick={handleSort}
+            column="EventNature">
+            Event Nature
+            {listAttributes.sortBy === "EventNature" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span
+            class="status sort-column"
+            onClick={handleSort}
+            column="PresenterType">
+            Presenter Type
+            {listAttributes.sortBy === "PresenterType" &&
+              (listAttributes.sortDirection ? (
+                <FiArrowUp className="ml-2" />
+              ) : (
+                <FiArrowDown className="ml-2" />
+              ))}
+          </span>
+          <span class="edit sort-column" column="PresenterType">
+            Edit
+          </span>
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          displayList.map((item) => (
+            <EventItem event={item} key={item.EventId} match={match} />
+          ))
+        )}
+      </div>
+      <ReactPaginate
+        previousLabel={"Prev"}
+        nextLabel={"Next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={listAttributes.pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={(x) =>
+          setListAttributes({ ...listAttributes, pageNumber: x.selected })
+        }
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
+    </div>
+  );
 }
 
 export default EventList;
--- src/components/core/Toast.js
+++ src/components/core/Toast.js
@@ -1,47 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { FiAlertCircle, FiCheckCircle, FiXCircle } from 'react-icons/fi';
-import { Toast, ToastBody, ToastHeader, Spinner } from 'reactstrap';
+import React, { useState, useEffect } from "react";
+import { FiAlertCircle, FiCheckCircle, FiXCircle } from "react-icons/fi";
+import { Toast, ToastBody, ToastHeader, Spinner } from "reactstrap";
 
 export const ToastStates = {
-    DEFAULT: 'DEFAULT',
-    LOADING: 'LOADING',
-    SUCCESS: 'SUCCESS',
-    FAIL: 'FAIL',
-}
+  DEFAULT: "DEFAULT",
+  LOADING: "LOADING",
+  SUCCESS: "SUCCESS",
+  FAIL: "FAIL",
+};
 
 const getClassName = (state) => {
-    switch (state) {
-        case ToastStates.SUCCESS : return 'success';
-        case ToastStates.FAIL : return 'fail';
-        case ToastStates.LOADING : return 'loading';
-        default : return 'default';
+  switch (state) {
+    case ToastStates.SUCCESS:
+      return "success";
+    case ToastStates.FAIL:
+      return "fail";
+    case ToastStates.LOADING:
+      return "loading";
+    default:
+      return "default";
+  }
+};
+
+const CustomToast = ({
+  show,
+  toastState = ToastStates.DEFAULT,
+  title,
+  message,
+  onClose,
+}) => {
+  useEffect(() => {
+    if (show) {
+      setTimeout(() => {
+        if (onClose !== undefined) onClose();
+      }, 3000);
     }
-}
+  }, [show]);
 
-const CustomToast = ({show, toastState = ToastStates.DEFAULT, title, message, onClose}) => {
-    
-    useEffect(() => {
-        console.log('Showing Toast');
-        if(show) {
-            setTimeout(() => {
-                if(onClose !== undefined) onClose();
-                console.log('Hiding Toast');
-            }, 3000)
-        }},[show]
-    )
-
-    return (
-        show === true && <div className={getClassName(toastState)}>
-            <div class="toast fade show" role="alert">
-                <div class="toast-header">
-                    <strong class="mr-auto"> 
-                        {toastState === ToastStates.SUCCESS ? <FiCheckCircle className="mr-1"/> : <FiAlertCircle className="mr-1"/>} {title}
-                    </strong>
-                </div>
-                <div class="toast-body">{message}</div>
-            </div>
+  return (
+    show === true && (
+      <div className={getClassName(toastState)}>
+        <div class="toast fade show" role="alert">
+          <div class="toast-header">
+            <strong class="mr-auto">
+              {toastState === ToastStates.SUCCESS ? (
+                <FiCheckCircle className="mr-1" />
+              ) : (
+                <FiAlertCircle className="mr-1" />
+              )}{" "}
+              {title}
+            </strong>
+          </div>
+          <div class="toast-body">{message}</div>
         </div>
+      </div>
     )
-}
+  );
+};
 
-export default CustomToast
+export default CustomToast;
